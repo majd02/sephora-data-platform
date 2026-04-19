9131824cd734d9eb9eae8f881de0b3ed28e97794
majd02 <majdidani02@gmail.com>
2026-04-19 20:03:32 +0100

    implementation des services Materiels : Parseur XML

--- src/main/java/com/sephora/data/model/Material.java
+++ src/main/java/com/sephora/data/model/Material.java
@@ -1,4 +1,26 @@
 package com.sephora.data.model;
 
 public class Material {
+    private int id;
+    private int productId;
+    private String matiere;
+    private String fournisseur;
+    private String certification; // BIO, VEGAN, CRUELTY_FREE, NONE
+
+    public Material() {}
+
+    public Material(int id, int productId, String matiere, String fournisseur, String certification) {
+        this.id = id;
+        this.productId = productId;
+        this.matiere = matiere;
+        this.fournisseur = fournisseur;
+        this.certification = certification;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public int getProductId() { return productId; }
+    public String getMatiere() { return matiere; }
+    public String getFournisseur() { return fournisseur; }
+    public String getCertification() { return certification; }
 }
